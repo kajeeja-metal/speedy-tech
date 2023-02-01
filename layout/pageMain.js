101a93447479bb0709370a277951b5ff32207b23
kajeeja-metal <45136475+kajeeja-metal@users.noreply.github.com>
2023-02-01 17:30:23 +0700

    Update pageMain.js

--- layout/pageMain.js
+++ layout/pageMain.js
@@ -24,7 +24,7 @@ const Pages = (props) => {
         return () => document.body.removeEventListener('wheel', cancelWheel)
     }, [])
     return (
-        <div className={props.isDetail == true ? 'contentArea hidden' : 'contentArea'} >
+        <div className={props.isDetail == true ? 'contentArea hidden' : 'contentArea'} onWheel={e => onWhee(e)} >
             <Header setSearch={props.setSearch} />
             <div className="contentArea_wrapper">
                 {props.children}
